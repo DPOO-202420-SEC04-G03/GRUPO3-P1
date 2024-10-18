453e84e54c272aa7ef9b2bd0d2c754b611668655
juanmoreno2211 <ja.moreno2@uniandes.edu.co>
2024-10-18 09:00:28 -0500

    yo soy tu gatito miau

---
+++ src/persistencia/JsonPersistenciaActividades.java
@@ -0,0 +1,19 @@
+package persistencia;
+import java.util.Objects;
+
+public class JsonPersistenciaActividades {
+
+    private String rutaArchivo;
+
+
+
+
+
+    public JsonPersistenciaActividades(String rutaArchivo) {
+        this.rutaArchivo = rutaArchivo;
+    }
+    
+
+
+    
+}
