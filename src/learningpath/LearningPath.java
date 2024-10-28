50eec0fa1b3c5de154bb91d322f4846c0f2ee9b8
juanpacai168 <jp.caicedoc1@uniandes.edu.co>
2024-10-27 20:34:31 -0500

    DAS
    KJDF0SOKFDSP

--- src/learningpath/LearningPath.java
+++ src/learningpath/LearningPath.java
@@ -1,204 +1,168 @@
-package learningpath;
+    package learningpath;
+    import java.util.ArrayList;
+    import java.util.Date;
+    import java.util.List;
+    import actividad.Actividad;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-import actividad.Actividad;
+    //Atributos
+    public class LearningPath {                
+        private String titulo;
+        private String descripcion;
+        private List<String> objetivos;
+        private String nivel_dificultad;    // Principiante/Intermedio/Avanzado
+        private int id_LP;
+        private int duracion;
+        private double rating;
+        private Date fecha_creacion;
+        private Date fecha_modificacion;
+        private String version;
+        private List<Actividad> actividades;
 
-// Atributos
-public class LearningPath {       
-    private String titulo;
-    private String descripcion;
-    private Set<Actividad> actividades; 
-    private String nivel_dificultad;    // Principiante/Intermedio/Avanzado
-    private int id_LP;
-    private int duracion;
-    private List<Double> ratings; // Almacena múltiples calificaciones
-    private Date fecha_creacion;
-    private Date fecha_modificacion;
-    private String version;
-    private String objetivo;  
-
-    // Constructor
-    public LearningPath(String titulo, String descripcion, Set<Actividad> actividades, String nivel_dificultad, int id_LP, Date fecha_creacion, String version, String objetivo) {
-        this.titulo = titulo;
-        this.descripcion = descripcion;
-        this.actividades = new HashSet<>(actividades);
-        this.nivel_dificultad = nivel_dificultad;
-        this.id_LP = id_LP;
-        this.fecha_creacion = fecha_creacion;
-        this.version = version;
-        this.duracion = 0; // Inicialmente 0 hasta que se añadan actividades
-        this.ratings = new ArrayList<>(); // Inicializa una lista vacía de calificaciones
-        this.fecha_modificacion = new Date(); // Inicializa con la fecha actual
-        this.objetivo = objetivo; 
-        actualizarDuracion();
-    }
-
-    // Setters y Getters
-    public String getTitulo() {
-        return this.titulo;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public String getDescripcion() {
-        return this.descripcion;
-    }
-
-    public void setDescripcion(String descripcion) {
-        this.descripcion = descripcion;
-    }
-
-    public Set<Actividad> getActividades() {
-        return this.actividades;
-    }
-
-    public void setActividades(Set<Actividad> actividades) {
-        this.actividades = actividades;
-    }
-
-    public String getNivel_dificultad() {
-        return this.nivel_dificultad;
-    }
-
-    public void setNivel_dificultad(String nivel_dificultad) {
-        this.nivel_dificultad = nivel_dificultad;
-    }
-
-    public int getId_LP() {
-        return this.id_LP;
-    }
-
-    public void setId_LP(int id_LP) {
-        this.id_LP = id_LP;
-    }
-
-    public int getDuracion() {
-        return this.duracion;
-    }
-
-    public void setDuracion(int duracion) {
-        this.duracion = duracion;
-    }
-
-    public double getRating() {
-
-        if (this.ratings.isEmpty()) {
-
-            return 0.0; 
+        //constructor
+        public LearningPath(String titulo, String descripcion, List<String> objetivos, String nivel_dificultad, int id_LP, Date fecha_creacion, String version) {
+        
+            this.titulo = titulo;
+            this.descripcion = descripcion;
+            this.objetivos = objetivos;
+            this.nivel_dificultad = nivel_dificultad;
+            this.id_LP = id_LP;
+            this.fecha_creacion = fecha_creacion;
+            this.version = version;
+            this.duracion = 0; // Inicialmente 0 hasta que se añadan actividades
+            this.rating = 0.0; // Inicialmente el rating es 0
+            this.fecha_modificacion = new Date(); // Inicializa con la fecha actual
+            this.actividades= new ArrayList<>();
         }
-        double ratingtotal = 0;
+        //setters y getters
 
-        for (double rating : this.ratings) {
-            ratingtotal += rating;
+        public String getTitulo() {
+            return this.titulo;
         }
-        return ratingtotal / this.ratings.size(); 
-    }
 
-    public Date getFecha_creacion() {
-        return this.fecha_creacion;
-    }
-
-    public void setFecha_creacion(Date fecha_creacion) {
-        this.fecha_creacion = fecha_creacion;
-    }
-
-    public Date getFecha_modificacion() {
-        return this.fecha_modificacion;
-    }
-
-    public void setFecha_modificacion(Date fecha_modificacion) {
-        this.fecha_modificacion = fecha_modificacion;
-    }
-
-    public String getVersion() {
-        return this.version;
-    }
-
-    public void setVersion(String version) {
-        this.version = version;
-    }
-
-    public String getObjetivo() {
-        return this.objetivo;
-    }
-
-    public void setObjetivo(String objetivo) {
-        this.objetivo = objetivo;
-    }
-
-    // Métodos
-
-    public void agregarActividad(Actividad actividad) {
-        this.actividades.add(actividad); 
-        actualizarDuracion();
-        this.fecha_modificacion = new Date();
-    }
-    
-    private void actualizarDuracion() {
-        int duracion_total = 0;
-        for (Actividad actividad : this.actividades) {
-            duracion_total += actividad.getDuracion();
+        public void setTitulo(String titulo) {
+            this.titulo = titulo;
         }
-        this.duracion = duracion_total;
-    }
 
-    public void eliminarActividad(Actividad actividad) {
-        this.actividades.remove(actividad);
-        actualizarDuracion();
-        this.fecha_modificacion = new Date();
-    }
-
-    public int cantidadActividades() {
-        return this.actividades.size();
-    }
-
-    public void editarActividad(Actividad antigua, Actividad nueva) {
-        if(this.actividades.remove(antigua)) { 
-            this.actividades.add(nueva); 
-            actualizarDuracion();
-            this.fecha_modificacion = new Date(); 
+        public String getDescripcion() {
+            return this.descripcion;
         }
-    }
 
-    public void agregarRating(double nuevoRating) {
-        // Añade un nuevo rating a la lista y actualiza la fecha de modificación
-        this.ratings.add(nuevoRating);
-        this.fecha_modificacion = new Date();
-    }
-
-    // Métodos estáticos para búsqueda, edición y eliminación de Learning Paths
-    public static LearningPath buscarLearningPath(Set<LearningPath> learningPaths, int id_LP) {
-        for (LearningPath lp : learningPaths) {
-            if (lp.getId_LP() == id_LP) {
-                return lp;
-            }
+        public void setDescripcion(String descripcion) {
+            this.descripcion = descripcion;
         }
-        return null;
-    }
 
-    public static void editarLearningPath(Set<LearningPath> learningPaths, int id_LP, String nuevoTitulo, String nuevaDescripcion, String nuevoObjetivo, 
-                                            String nuevoNivelDificultad, String nuevaVersion) {
-        LearningPath learningPath = buscarLearningPath(learningPaths, id_LP);
-        if (learningPath != null) {
-            learningPath.setTitulo(nuevoTitulo);
-            learningPath.setDescripcion(nuevaDescripcion);
-            learningPath.setObjetivo(nuevoObjetivo);
-            learningPath.setNivel_dificultad(nuevoNivelDificultad);
-            learningPath.setVersion(nuevaVersion);
-            learningPath.setFecha_modificacion(new Date());  
+        public List<String> getObjetivos() {
+            return this.objetivos;
         }
-    }
 
-    public static void eliminarLearningPath(Set<LearningPath> learningPaths, int id_LP) {
-        LearningPath learningPath = buscarLearningPath(learningPaths, id_LP);
-        if (learningPath != null) {
-            learningPaths.remove(learningPath);  
+        public void setObjetivos(List<String> objetivos) {
+            this.objetivos = objetivos;
         }
-    }
+
+        public String getNivel_dificultad() {
+            return this.nivel_dificultad;
+        }
+
+        public void setNivel_dificultad(String nivel_dificultad) {
+            this.nivel_dificultad = nivel_dificultad;
+        }
+
+        public int getId_LP() {
+            return this.id_LP;
+        }
+
+        public void setId_LP(int id_LP) {
+            this.id_LP = id_LP;
+        }
+
+        public int getDuracion() {
+            return this.duracion;
+        }
+
+        public void setDuracion(int duracion) {
+            this.duracion = duracion;
+        }
+
+        public double getRating() {
+            return this.rating;
+        }
+
+        public void setRating(double rating) {
+            this.rating = rating;
+            this.fecha_modificacion = new Date();
 }
+
+        public Date getFecha_creacion() {
+            return this.fecha_creacion;
+        }
+
+        public void setFecha_creacion(Date fecha_creacion) {
+            this.fecha_creacion = fecha_creacion;
+        }
+
+        public Date getFecha_modificacion() {
+            return this.fecha_modificacion;
+        }
+
+        public void setFecha_modificacion(Date fecha_modificacion) {
+            this.fecha_modificacion = fecha_modificacion;
+        }
+
+        public String getVersion() {
+            return this.version;
+        }
+
+        public void setVersion(String version) {
+            this.version = version;
+        }
+
+        public List<Actividad> getActividades() {
+            return this.actividades;
+        }
+
+        public void setActividades(List<Actividad> actividades) {
+        this.actividades = actividades;
+        }
+        
+        
+        
+        // métodos
+
+        public void agregarActividad(Actividad actividad){
+
+            this.actividades.add(actividad);  
+            actualizarDuracion();
+            this.fecha_modificacion = new Date();
+        }
+        
+        private void actualizarDuracion(){
+
+            int duracion_total= 0;
+            for (Actividad actividad: this.actividades){
+
+                duracion_total+= actividad.getDuracion();
+            }
+            this.duracion= duracion_total;
+
+        }
+
+        public void eliminarActividad(Actividad actividad){
+
+
+            this.actividades.remove(actividad);
+            
+            actualizarDuracion();
+
+            this.fecha_modificacion= new Date();
+
+        }
+
+        public int cantidadActividades(){
+
+            return this.actividades.size();
+
+        }
+    }
+
+    
+
